7bc968dffc8b5d1f0b7dbd1ad164e2194010b8be
Juan Suarez <juansuarezbanol@gmail.com>
2023-02-14 17:53:37 -0500

    first commit

---
+++ first-project/first-project/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import { HeaderHome } from './components/layouts/HeaderHome/HeaderHome'
+
+
+function App() {
+  return (
+    <>
+      <HeaderHome />
+    </>
+  );
+
+}
+
+export default App;
